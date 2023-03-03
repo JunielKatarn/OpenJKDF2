e08bca3828726afeeb4e5f07e43346fef0129d0d
Julio C. Rocha <dev@rocha.red>
2023-03-03 01:37:51 -0800

    Format build_win64_msvc.sh

--- build_win64_msvc.sh
+++ build_win64_msvc.sh
@@ -10,9 +10,10 @@ mkdir -p build_win64 && cd build_win64
 
 if [ ! -f build_protoc/protoc ]; then
     mkdir -p build_protoc && cd build_protoc
-    cmake   -DCMAKE_BUILD_TYPE=Release \
-            -Dprotobuf_BUILD_TESTS=OFF \
-            ../../3rdparty/protobuf/cmake
+    cmake                                   \
+        -DCMAKE_BUILD_TYPE=Release          \
+        -Dprotobuf_BUILD_TESTS=OFF          \
+        ../../3rdparty/protobuf/cmake
     make -j10 protoc
     cd ..
 fi
@@ -26,12 +27,12 @@ C_INCLUDE_PATH=""
 if [ ! -f build_protobuf/libprotobuf.dll ]; then
     mkdir -p build_protobuf && cd build_protobuf
     PB_BUILD=$(pwd)
-    cmake   \
-        --toolchain ../../cmake_modules_windows/Windows.MSVC.toolchain.cmake   \
-        -DCMAKE_INSTALL_PREFIX=$PB_BUILD                        \
-        -DCMAKE_BUILD_TYPE=Release                              \
-        -Dprotobuf_BUILD_TESTS=OFF                              \
-        -Dprotobuf_BUILD_SHARED_LIBS=ON                         \
+    cmake                                                                               \
+        --toolchain $PB_BUILD/../../cmake_modules_windows/Windows.MSVC.toolchain.cmake  \
+        -DCMAKE_INSTALL_PREFIX=$PB_BUILD                                                \
+        -DCMAKE_BUILD_TYPE=Release                                                      \
+        -Dprotobuf_BUILD_TESTS=OFF                                                      \
+        -Dprotobuf_BUILD_SHARED_LIBS=ON                                                 \
         ../../3rdparty/protobuf/cmake
     make -j10 install
     cd ..
@@ -40,7 +41,8 @@ fi
 if [ ! -f build_gns/bin/libGameNetworkingSockets.dll ]; then
     mkdir -p build_gns && cd build_gns
     GNS_BUILD=$(pwd)
-    cmake --toolchain $GNS_BUILD/../../cmake_modules_windows/Windows.MSVC.toolchain.cmake                  \
+    cmake                                                                                   \
+        --toolchain $GNS_BUILD/../../cmake_modules_windows/Windows.MSVC.toolchain.cmake     \
         -DCMAKE_BUILD_TYPE=Release                                                          \
         -DProtobuf_USE_STATIC_LIBS=ON                                                       \
         -DProtobuf_LIBRARIES="-L$GNS_BUILD/../build_protobuf/lib"                           \
@@ -58,24 +60,26 @@ fi
 if [ ! -f build_physfs/libphysfs.a ]; then
     mkdir -p build_physfs && cd build_physfs
     PHYSFS_BUILD=$(pwd)
-    cmake   --toolchain $PHYSFS_BUILD/../../cmake_modules_windows/Windows.MSVC.toolchain.cmake \
-        -DCMAKE_BUILD_TYPE=Release                                              \
-        -DPHYSFS_ARCHIVE_GRP=FALSE                                              \
-        -DPHYSFS_ARCHIVE_WAD=FALSE                                              \
-        -DPHYSFS_ARCHIVE_HOG=FALSE                                              \
-        -DPHYSFS_ARCHIVE_MVL=FALSE                                              \
-        -DPHYSFS_ARCHIVE_QPAK=FALSE                                             \
-        -DPHYSFS_ARCHIVE_SLB=FALSE                                              \
-        -DPHYSFS_ARCHIVE_VDF=FALSE                                              \
+    cmake                                                                                   \
+        --toolchain $PHYSFS_BUILD/../../cmake_modules_windows/Windows.MSVC.toolchain.cmake  \
+        -DCMAKE_BUILD_TYPE=Release                                                          \
+        -DPHYSFS_ARCHIVE_GRP=FALSE                                                          \
+        -DPHYSFS_ARCHIVE_WAD=FALSE                                                          \
+        -DPHYSFS_ARCHIVE_HOG=FALSE                                                          \
+        -DPHYSFS_ARCHIVE_MVL=FALSE                                                          \
+        -DPHYSFS_ARCHIVE_QPAK=FALSE                                                         \
+        -DPHYSFS_ARCHIVE_SLB=FALSE                                                          \
+        -DPHYSFS_ARCHIVE_VDF=FALSE                                                          \
         $PHYSFS_BUILD/../../3rdparty/physfs
     make -j10
     cd ..
 fi
 
 #cmake .. --toolchain ../cmake_modules_windows/Windows.MSVC.toolchain.cmake -D USE_CRYPTO="BCrypt" -Dprotobuf_BUILD_TESTS=OFF
-cmake   ..  \
-    --toolchain ../cmake_modules_windows/Windows.MSVC.toolchain.cmake  \
-    &&  \
+cmake                                                                   \
+    ..                                                                  \
+    --toolchain ../cmake_modules_windows/Windows.MSVC.toolchain.cmake   \
+    &&                                                                  \
     make -j10 openjkdf2-64
 cd ..
 
